9ad1bc593a80302cdf19e29ec85cf1b3b89b6c3c
ravenusmc <mcuddy77@gmail.com>
2023-08-06 11:54:43 -0400

    deleted folders not needed

--- ai/one.py
+++
@@ -1,9 +0,0 @@
-import openai
-from dotenv import dotenv_values 
-
-config = dotenv_values(".env")
-# print(config["OPENAI_API_KEY"])
-
-openai.api_key = config["OPENAI_API_KEY"]
-test = openai.Completion.create(model='text-davinci-003', prompt="Happy Birthday ")
-print(test)
